85fbdbd32245ff4d834520015be98c2230736cf6
Rohan Dhar <deusexmachina892@gmail.com>
2023-11-20 16:30:42 +0530

    config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const {PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+   defaultNetwork: "mumbai",
+   networks: {
+     hardhat: {},
+     mumbai: {
+       url: "https://rpc.ankr.com/polygon_mumbai",
+       accounts: [`0x${PRIVATE_KEY}`],
+     },
+   },
 };
 
 export default config;
